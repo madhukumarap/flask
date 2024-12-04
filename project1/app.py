83366de1099e2a98466d65f25b1df9e8e915b743
madhukumarap <madhukumarap07@gmail.com>
2024-12-04 22:04:04 +0530

    jinja and dynamically use the html

--- project1/app.py
+++ project1/app.py
@@ -1,14 +1,15 @@
 from flask import Flask, redirect, url_for, render_template
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello welcome to the my flask project"
+    return "Hello, welcome to my Flask project!"
 
 @app.route('/home/<name>')
 def home(name):
-    return render_template('index.html', name=name)
+    # Pass the `name` parameter and a list of names to the template
+    return render_template('index.html', current_name=name, names=['name', 'admin'])
+
 if __name__ == '__main__':
     app.run(debug=True)
